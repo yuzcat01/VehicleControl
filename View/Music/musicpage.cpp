86aac6372f91a56b1e75dcf186b08e22412370e8
fishshi <2855691008@qq.com>
2024-08-24 20:21:35 +0800

    音乐页面进度条移动，时间变化等，及bug修复

--- View/Music/musicpage.cpp
+++ View/Music/musicpage.cpp
@@ -10,18 +10,26 @@ MusicPage::MusicPage(QWidget *parent)
     , mediaPlaybackPause(new QIcon(QIcon::fromTheme("media-playback-pause")))
 {
     ui->setupUi(this);
+    player->setAudioOutput(new QAudioOutput(this));
+
+    ui->musicSlider->installEventFilter(this);
+
+    connect(player, &QMediaPlayer::positionChanged, this, &MusicPage::do_positionChanged);
+    connect(player, &QMediaPlayer::durationChanged, this, &MusicPage::do_durationChanged);
 }
 
 MusicPage::~MusicPage()
 {
     delete ui;
     delete player;
+    delete mediaPlaybackStart;
+    delete mediaPlaybackPause;
 }
+
 void MusicPage::on_backToHome_clicked()
 {
     emit toHome();
 }
-
 
 void MusicPage::on_playBtn_clicked()
 {
@@ -34,6 +42,8 @@ void MusicPage::on_playBtn_clicked()
     {
         if(ui->musicList->count() <= 0)
             return;
+        if(ui->musicList->currentRow() < 0)
+            ui->musicList->setCurrentRow(0);
         ui->playBtn->setIcon(*mediaPlaybackPause);
         if(player->playbackState() == QMediaPlayer::PausedState)
         {
@@ -77,7 +87,6 @@ void MusicPage::on_musicBtn_clicked()
     ui->stackedWidget->setCurrentIndex(1);
 }
 
-
 void MusicPage::on_musicListBtn_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
@@ -102,13 +111,6 @@ void MusicPage::on_addBtn_clicked()
         aItem->setData(Qt::UserRole, QUrl::fromLocalFile(item));
         ui->musicList->addItem(aItem);
     }
-
-    if(player->playbackState() != QMediaPlayer::PlayingState){
-        ui->musicList->setCurrentRow(0);
-        QUrl source = ui->musicList->currentItem()->data(Qt::UserRole).value<QUrl>();
-        player->setSource(source);
-        player->play();
-    }
 }
 
 void MusicPage::on_removeBtn_clicked()
@@ -117,10 +119,101 @@ void MusicPage::on_removeBtn_clicked()
     if(index < 0)
         return;
     delete ui->musicList->takeItem(index);
+    if(ui->musicList->currentRow() >= 0)
+    {
+        player->setSource(ui->musicList->currentItem()->data(Qt::UserRole).value<QUrl>());
+        player->play();
+        ui->playBtn->setIcon(*mediaPlaybackPause);
+    }
+    else
+    {
+        player->stop();
+        ui->playBtn->setIcon(*mediaPlaybackStart);
+    }
 }
 
 void MusicPage::on_clearBtn_clicked()
 {
     ui->musicList->clear();
     player->stop();
+    ui->playBtn->setIcon(*mediaPlaybackStart);
+}
+
+void MusicPage::do_positionChanged(qint64 position)
+{
+    if(ui->musicSlider->isSliderDown())
+        return;
+    ui->musicSlider->setSliderPosition(position);
+    int secs = position/1000;
+    int mins = secs/60;
+    secs %= 60;
+    positionTime = QString::asprintf("%d:%02d", mins, secs);
+    ui->nowTime->setText(positionTime);
+    ui->allTime->setText(durationTime);
+}
+
+void MusicPage::do_durationChanged(qint64 duration)
+{
+    ui->musicSlider->setMaximum(duration);
+    int secs = duration / 1000;
+    int mins = secs / 60;
+    secs %= 60;
+    durationTime = QString::asprintf("%d:%02d", mins, secs);
+    ui->nowTime->setText(positionTime);
+    ui->allTime->setText(durationTime);
+}
+
+bool MusicPage::eventFilter(QObject *watched, QEvent *event)
+{
+    // 处理滑块点击事件
+    if (event->type() == QEvent::MouseButtonPress && watched == ui->musicSlider) {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+        // 计算鼠标点击位置对应的值
+        if (mouseEvent->button() == Qt::LeftButton)	//判断左键
+        {
+            int clickedValue = ui->musicSlider->minimum() + ((ui->musicSlider->maximum() - ui->musicSlider->minimum()) * mouseEvent->pos().x() / ui->musicSlider->width());
+            qDebug() << "触发了点击事件，位置为:" << clickedValue << Qt::endl;
+            ui->musicSlider->setValue(clickedValue);
+            player->setPosition(clickedValue); // 设置播放器位置
+            return true;
+        }
+    } else if (event->type() == QEvent::MouseMove && watched == ui->musicSlider) {
+        qDebug() << "触发了移动事件" << Qt::endl;
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+        if (mouseEvent->buttons() & Qt::LeftButton) {
+            int draggedValue = ui->musicSlider->minimum() + ((ui->musicSlider->maximum() - ui->musicSlider->minimum()) * mouseEvent->pos().x() / ui->musicSlider->width());
+            ui->musicSlider->setValue(draggedValue); // 更新滑块的值
+            player->setPosition(draggedValue); // 设置播放器位置
+            return true;
+        }
+    }
+    return QWidget::eventFilter(watched, event);
+}
+
+void MusicPage::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        isDragging = true;
+        lastMousePosition = event->globalPos() - frameGeometry().topLeft();
+        event->accept();
+    }
+    QWidget::mousePressEvent(event);
+}
+
+void MusicPage::mouseMoveEvent(QMouseEvent *event)
+{
+    if (isDragging && (event->buttons() & Qt::LeftButton)) {
+        move(event->globalPos() - lastMousePosition);
+        event->accept();
+    }
+    QWidget::mouseMoveEvent(event);
+}
+
+void MusicPage::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        isDragging = false;
+        event->accept();
+    }
+    QWidget::mouseReleaseEvent(event);
 }
--- View/Music/musicpage.h
+++ View/Music/musicpage.h
@@ -16,6 +16,13 @@ public:
     explicit MusicPage(QWidget *parent = nullptr);
     ~MusicPage();
 
+    virtual bool eventFilter(QObject *watched, QEvent *event) override;
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
 signals:
     void toHome();
 
@@ -23,27 +30,32 @@ private slots:
     void on_backToHome_clicked();
 
     void on_playBtn_clicked();
-
     void on_nextBtn_clicked();
-
-    void on_musicBtn_clicked();
-
-    void on_musicListBtn_clicked();
-
     void on_previousBtn_clicked();
 
+    void on_musicBtn_clicked();
+    void on_musicListBtn_clicked();
+
     void on_addBtn_clicked();
-
     void on_removeBtn_clicked();
-
     void on_clearBtn_clicked();
+
+    void do_positionChanged(qint64 position);
+    void do_durationChanged(qint64 duration);
 
 private:
     Ui::MusicPage *ui;
+
     QMediaPlayer *player;
 
     QIcon *mediaPlaybackStart;
     QIcon *mediaPlaybackPause;
+
+    QString positionTime;
+    QString durationTime;
+
+    bool isDragging;
+    QPoint lastMousePosition;
 };
 
 #endif // MUSICPAGE_H
