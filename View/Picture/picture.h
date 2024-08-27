1e641430495cdd2414b33c720cd2beef5fe1457b
cat yu <yucat01miao@qq.com>
2024-08-28 01:02:02 +0800

    增加图片查看功能

--- View/Picture/picture.h
+++ View/Picture/picture.h
@@ -2,6 +2,15 @@
 #define PICTURE_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QFileDialog>
+#include <QScrollArea>
+#include <QLabel>
+#include <QPixmap>
+#include <QList>
+#include <QMouseEvent> // For handling mouse
 
 namespace Ui {
 class Picture;
@@ -21,8 +30,27 @@ signals:
 private slots:
     void on_toHome_clicked();
 
+    void showImage(int index);
+
+    void on_selectButton_clicked();
+
+    void on_prevButton_clicked();
+
+    void on_nextButton_clicked();
+
+    void on_removeButton_clicked();
+
 private:
     Ui::Picture *ui;
+    QList<QString> imagePaths; // Stores the paths of selected images
+    int currentIndex = 0;
+
+    //QWidget *previewWidget;     // Container for thumbnails
+    //QHBoxLayout *previewLayout; // Layout for the thumbnails
+    //QScrollArea *previewArea;   // Scroll area for the thumbnails
+
+    //void updateImageDisplay();
+    void updateThumbnailList();
 };
 
 #endif // PICTURE_H
